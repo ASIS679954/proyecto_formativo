7fd61f3fae25410969936c9b1791200fab78331a
DanielCeron <datutalcha3@misena.edu.co>
2015-08-21 17:02:02 -0500

    modelos

--- sif/sif/apps/home/models.py
+++ sif/sif/apps/home/models.py
@@ -1,22 +1,36 @@
+from django.db import models
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
 
+# Create your models here.
 from django.db import models
 
-class Usuario(models.Model):
-	genero = (
-		   (u'M',u'masculino'),
-		   (u'F',u'femenino'),
-		   (u'Otro',u'otro'),
-		)
-	nombre = models.CharField(max_length=50)
-	apellido = models.CharField(max_length=50)
-	email = models.CharField(max_length=30)
-	direccion = models.CharField(max_length=30)
-	telefono = models.CharField(max_length=20)
-	sexo = models.CharField(max_length=15, choices = genero)
+
+class Departamento(models.Model):
+	nombre 		= models.CharField(max_length=50)
+	municipio 	= models.CharField(max_length=50)
 
 	def __unicode__(self):
 		return self.nombre
 
+class Usuario(models.Model):
+	generos = (
+			(u'mascu',u'Masculino'),
+			(u'feme',u'Femenino'),
+			(u'otro',u'Otro'),
+		)
+	identificacion  = models.CharField(max_length = 20, unique = True)
+	nombre 			= models.CharField(max_length=50)
+	apellido 		= models.CharField(max_length=50)
+	#email			= models.CharField(max_length=30)
+	direccion 		= models.CharField(max_length=30)
+	telefono 		= models.CharField(max_length=20)
+	sexo 			= models.CharField(max_length=15, choices = generos, default = "Masculino")
+    #clave 			= models.CharField(max_length=20)
+    #estado 		= models.BooleanField(default = True)
+    #userito 		= 
+	def __unicode__(self):
+		return self.nombre
 
 class Rol(models.Model):
 	nombre = models.CharField(max_length=50)
@@ -25,73 +39,57 @@ class Rol(models.Model):
 	def __unicode__(self):
 		return self.nombre
 
-class Producto(models.Model):
+class Proveedor(models.Model):
+	identificacion = models.CharField(max_length=50, unique = True)
 	nombre = models.CharField(max_length=50)
-	fecha_ingreso = models.DateField()
-	ancho = models.CharField(max_length=10)
-	largo = models.CharField(max_length=10)
+	apellido = models.CharField(max_length=50)
+	telefono = models.CharField(max_length=50)
+	direccion = models.CharField(max_length=50)
+	email = models.EmailField()
+	razon_social = models.CharField(max_length=50)
 
 	def __unicode__(self):
 		return self.nombre
 
-class Departamento(models.Model):
-	nombre = models.CharField(max_length=50)
-	
-	def __unicode__(self):
-		return self.nombre	
-
-class Municipio(models.Model):
-	nombre = models.CharField(max_length=50)
-	departamento = models.ForeignKey(Departamento)
-
-
-	def __unicode__(self):
-		return self.nombre			
-
-
-class Proveedor(models.Model):
-	identificacion = models.CharField(max_length=50)
-	nombre = models.CharField(max_length=50)
-	telefono = models.CharField(max_length=50)
-	direccion = models.CharField(max_length=50)
-	email = models.CharField(max_length=50)
-	municipio = models.ForeignKey(Municipio)
-	producto = models.ForeignKey(Producto)
-
-	def __unicode__(self):
-		return self.identificacion
-
-
-class Transferencia(models.Model):
-	fecha_transferencia = models.DateField()
-
-
-
-	def __unicode__(self):
-		return str(self.fecha_transferencia)
-
-
-class Sede(models.Model):
-	municipio = models.ForeignKey(Municipio)
-	direccion = models.CharField(max_length=30)
-	telefono = models.CharField(max_length=20)
-	producto = models.ForeignKey(Producto)
-	transferencia = models.DateField(
-)	#usuario = models.ForeignKey(Usuario)
-
-	def __unicode__(self):
-		return self.municipio.nombre
-
-
-
 class CodigoBarras(models.Model):
 	codigo = models.CharField(max_length=13)
-	fecha = models.DateField()
+	fecha = models.DateField(auto_now = True)
 
 	def __unicode__(self):
 		return self.codigo
 
+class Producto(models.Model):
+	nombre = models.CharField(max_length=50)
+	referencia = models.CharField(max_length=50)
+	fecha_ingreso = models.DateField()
+	ancho = models.CharField(max_length=10)
+	largo = models.CharField(max_length=10)
+	proveedor = models.ForeignKey(Proveedor)
+	codigobarras = models.ForeignKey(CodigoBarras)
+	valor = models.IntegerField()
+	descripcion = models.TextField(max_length=150)
+	cantidad	= models.IntegerField(default = 0)
 
+	def __unicode__(self):
+		return self.nombre
+
+class Sede(models.Model):
+	nombre_sede = models.CharField(max_length = 30)
+	direccion = models.CharField(max_length=30)
+	telefono = models.CharField(max_length=20)
+	
+	def __unicode__(self):
+		return self.nombre_sede
+
+class Salida(models.Model):
+	fecha_salida = models.DateField(auto_now = True)
+	tipo_salida = models.CharField(max_length=50)
+	codigobarras = models.ForeignKey(CodigoBarras)
+	cantidad = models.IntegerField()
+
+
+	def __unicode__(self):
+		return str(self.fecha_salida)
 
 
 
