46010739e8407bdf4c7b1e657a4b580cf52010d7
Esteban <esteban22x@gmail.com>
2015-09-09 09:09:18 -0500

    Comprobacion si el id existe

--- sif/sif/apps/inventario/views.py
+++ sif/sif/apps/inventario/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from sif.apps.inventario.forms import FormuCrea
 from sif.apps.inventario.models import CodigoBarras
+from sif.apps.inventario.models import Producto
 from django.http import HttpResponseRedirect
 import barcode
 
@@ -15,7 +16,7 @@ def creaCodigo(request):
 		if formulario.is_valid():
 			EAN = barcode.get_barcode_class('ean13')
 			ean = EAN(request.POST.get('codigo'))
-			ean.save("nuevo")
+			ean.save(request.POST.get('codigo'))
  			agrega = formulario.save(commit = False)
 			agrega.save()
 			informacion = "Terminado"
@@ -24,3 +25,9 @@ def creaCodigo(request):
 		formulario = FormuCrea()
 		ctx = {'form': formulario,'info':informacion}
 	return render_to_response('inventario/agregaCB.html',ctx,context_instance = RequestContext(request))
+
+def ver_unico(request,id_cofre):
+	cofre = CodigoBarras.objects.get(id=id_cofre)
+	pp = Producto.objects.all().Codigobarras().count()
+	ctx = {'cofre':cofre,'cantidad':pp}
+	return render_to_response('inventario/muestraProducto.html',ctx,context_instance = RequestContext(request))
